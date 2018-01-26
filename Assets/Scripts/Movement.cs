2343cb070082d585a16bdcaaacfbdc4cf6fc788f
Kristian Sakarisson <kristian@sakarisson.com>
2018-01-26 22:11:01 +0200

    Added simple player prefab

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour {
+    public float speed = 10f;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    void Start () {
+        
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
 	}
 }
