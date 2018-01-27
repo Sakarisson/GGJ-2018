e84d2e48a4ac8d9b5bc126429cba6247d6accae4
Kristian Sakarisson <kristian@sakarisson.com>
2018-01-27 18:50:28 +0200

    Added player state script

---
+++ Assets/Scripts/PlayerState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Sex {
+    MALE,
+    FEMALE,
+};
+
+public class PlayerState : MonoBehaviour {
+    Sex initialState;
+    float tribe; // 0 - 1 where 0 is totally feminine and 1 is totally masculine
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
