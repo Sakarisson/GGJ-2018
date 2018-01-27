14f536e63cf193f0554dd8ccfcd11acdea937a56
Tuukka Koivisto <koivisto.tuukka@gmail.com>
2018-01-27 17:33:40 +0200

    Add bullets

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+
+	public float speed = 8f;
+	Vector3 direction;
+
+	public void setDirection(Vector3 _direction) {
+		direction = _direction;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += direction * speed * Time.deltaTime;
+	}
+
+	void OnBecameInvisible() {
+		Destroy (gameObject);
+	}
+}
