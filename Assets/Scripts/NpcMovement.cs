8e6493f9745b92d8ff0dd27c52b521817afcfd89
Tuukka Koivisto <koivisto.tuukka@gmail.com>
2018-01-27 13:28:25 +0200

    Rename npc movement

--- Assets/Scripts/NpcMovement.cs
+++ Assets/Scripts/NpcMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class NpcMovement : Movement {
 
+	float tarX = 0f;
+	float tarY = 0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,17 @@ public class NpcMovement : Movement {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (destinationReached())
+			setNewTarget();
+		move((new Vector2(tarX, tarY) - new Vector2(transform.position.x, transform.position.y)).normalized);
+	}
+
+	void setNewTarget() {
+		tarX = Random.Range(-5f, 5f);
+		tarY = Random.Range(-5f, 5f);
+	}
+
+	bool destinationReached() {
+		return System.Math.Abs(tarX - transform.position.x) < 0.5f && System.Math.Abs(tarY - transform.position.y) < 0.5f;
 	}
 }
