fb4631574731a5495e1a13ab49e8441daf7633d6
Gintautas Skersys <gintautas@hq.unity3d.com>
2018-01-27 21:34:03 +0200

    Fade transitions

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -11,54 +11,97 @@ public class ColorManager : MonoBehaviour
     public Color masculine;
     public Color feminine;
     public GameObject rootFace;
-    public float scaleTime = 5;
-    public float scaleSize = 5;
     public float fadeInTime = 60;
+
+    public Coroutine scaleRoot;
+    public Coroutine faceCenter;
+    public Coroutine fadeLeft;
+    public Coroutine fadeRight;
 
     void Start()
     {
         i = this;
-        StartCoroutine(ScaleRoot());
-        StartCoroutine(FadeIn(FeminineRenderer));
-        StartCoroutine(FadeIn(MasculineRenderer));
+        //ScaleRoot(5, 5);
+        FadeFeminine(false, 5);
+        FadeMasculine(false, 10);
+    }
+
+    public void ScaleRoot(float size, float time)
+    {
+        if (scaleRoot != null) StopCoroutine(scaleRoot);
+        scaleRoot = StartCoroutine(_ScaleRoot(size, time));
+    }
+
+    IEnumerator _ScaleRoot(float scale, float time)
+    {
+        Vector3 oldScale = rootFace.transform.localScale;
+        Vector3 newScale = Vector3.one * scale;
+
+        float t = 0f;
+        while (t < time)
+        {
+            t += Time.deltaTime;
+            rootFace.transform.localScale = Vector3.Slerp(oldScale, newScale, t / time);
+            yield return null;
+        }
     }
 
 
-    IEnumerator FadeIn(SpriteRenderer renderer)
+    public void FadeMasculine(bool visible, float time)
+    {
+        if (fadeLeft != null) StopCoroutine(fadeLeft);
+        if (visible)
+            fadeLeft = StartCoroutine(_FadeIn(time, MasculineRenderer));
+        else
+            fadeLeft = StartCoroutine(_FadeOut(time, MasculineRenderer));
+    }
+    public void FadePlayer(bool visible, float time)
+    {
+        if (fadeLeft != null) StopCoroutine(fadeLeft);
+        if (visible)
+            fadeLeft = StartCoroutine(_FadeIn(time, playerRenderer));
+        else
+            fadeLeft = StartCoroutine(_FadeOut(time, playerRenderer));
+    }
+    public void FadeFeminine(bool visible, float time)
+    {
+        if (fadeRight != null) StopCoroutine(fadeRight);
+        if (visible)
+            fadeRight = StartCoroutine(_FadeIn(time, FeminineRenderer));
+        else
+            fadeRight = StartCoroutine(_FadeOut(time, FeminineRenderer));
+    }
+
+    IEnumerator _FadeIn(float time, SpriteRenderer renderer)
     {
         float t = 0;
         Color32 startColor = Color.white;
         Color32 endColor = renderer.material.GetColor("_BottomColor");
         endColor.a = 0;
 
-        while (t < 1)
+        while (t < time)
         {
             t += Time.deltaTime / fadeInTime;
-            Color32 newColor = Color.Lerp(startColor, endColor, t);
-            renderer.material.SetColor("_BottomColor",newColor);
+            Color32 newColor = Color.Lerp(startColor, endColor, t / time);
+            renderer.material.SetColor("_BottomColor", newColor);
             yield return null;
         }
     }
 
-    IEnumerator ScaleRoot()
+    IEnumerator _FadeOut(float time, SpriteRenderer renderer)
     {
-        float t = scaleTime;
-        while (t > 1)
+        float t = 0;
+        Color32 startColor = renderer.material.GetColor("_BottomColor");
+        Color32 endColor = Color.white;
+        endColor.a = 0;
+
+        while (t < time)
         {
-            t -= Time.deltaTime;
-            ChangeScale(t);
+            t += Time.deltaTime;
+            Color32 newColor = Color.Lerp(startColor, endColor, t / time);
+            renderer.material.SetColor("_BottomColor", newColor);
             yield return null;
         }
     }
 
-    void ChangeScale(float value)
-    {
-        rootFace.transform.localScale = new Vector3(value, value, value);
-    }
-
-    public void ChangeColor(float t)
-    {
-        Color col = Color.Lerp(masculine, feminine, t);
-        playerRenderer.material.SetColor("_BottomColor", col);
-    }
 }
