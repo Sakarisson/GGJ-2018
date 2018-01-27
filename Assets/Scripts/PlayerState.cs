2514a03465db1b3f0e3877779ef1b4ca3f4a77a0
Kristian Sakarisson <kristian@sakarisson.com>
2018-01-27 20:04:45 +0200

    Added hit function for player

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -9,8 +9,9 @@ public enum Sex {
 
 public class PlayerState : MonoBehaviour {
     Sex initialState;
+    // Symbolizes gender identity
     float tribe; // 0 - 1 where 0 is totally feminine and 1 is totally masculine
-
+    float armor; // 
 
 	// Use this for initialization
 	void Start () {
@@ -21,4 +22,8 @@ public class PlayerState : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void PlayerWasHit(Sex type) {
+        
+    }
 }
