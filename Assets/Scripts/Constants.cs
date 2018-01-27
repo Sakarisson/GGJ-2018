9be419ad860af2b2aa06ef8cfb66bc9b7d2a0ee9
Kristian Sakarisson <kristian@sakarisson.com>
2018-01-27 14:32:23 +0200

    Added context related scripts

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour {
+    private Color[] allowedColors = {
+        new Color(79, 203, 225),
+        new Color(47, 142, 221),
+        new Color(214, 209, 47),
+        new Color(76, 213, 51),
+        new Color(255, 192, 20),
+        new Color(245, 14, 0),
+        new Color(224, 91, 82),
+        new Color(184, 178, 176),
+    };
+
+    public Color[] GetAllowedColors() {
+        return allowedColors;
+    }
+}
