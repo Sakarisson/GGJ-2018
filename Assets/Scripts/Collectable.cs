3c279bf6cde95fd613378a5750368e2bf24cc12a
Tuukka Koivisto <koivisto.tuukka@gmail.com>
2018-01-27 20:54:41 +0200

    Collectable self destroy

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -6,13 +6,17 @@ public class Collectable : MonoBehaviour {
 
 	public Sex sex;
 
+	float destroyAt;
+
 	// Use this for initialization
 	void Start () {
 		sex = Random.value < 0.5f ? Sex.FEMALE : Sex.MALE;
+		destroyAt = Time.time + 10f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Time.time >= destroyAt)
+			Destroy(gameObject);
 	}
 }
