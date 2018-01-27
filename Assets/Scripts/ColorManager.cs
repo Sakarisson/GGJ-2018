d63d988c313365b403d5c4946b925b62a31dcadc
Gintautas Skersys <gintautas@hq.unity3d.com>
2018-01-27 15:42:59 +0200

    Level changes

---
+++ Assets/Scripts/ColorManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    public static ColorManager i;
+    public SpriteRenderer middle;
+    public SpriteRenderer left;
+    public SpriteRenderer right;
+
+    void Start()
+    {
+        i = this;
+        middle.material.SetColor("_BottomColor", Constants.allowedColors[0]);
+        middle.material.SetColor("_TopColor", Constants.allowedColors[1]);
+        left.material.SetColor("_BottomColor", Constants.allowedColors[2]);
+        left.material.SetColor("_TopColor", Constants.allowedColors[3]);
+        right.material.SetColor("_BottomColor", Constants.allowedColors[4]);
+        right.material.SetColor("_TopColor", Constants.allowedColors[5]);
+    }
+}
--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Constants
 {
     public static Color[] allowedColors = {
-        new Color(79, 203, 225),
-        new Color(47, 142, 221),
-        new Color(214, 209, 47),
-        new Color(76, 213, 51),
-        new Color(255, 192, 20),
-        new Color(245, 14, 0),
-        new Color(224, 91, 82),
-        new Color(184, 178, 176),
+        new Color32(79, 203, 225, 255),
+        new Color32(47, 142, 221, 255),
+        new Color32(214, 209, 47, 255),
+        new Color32(76, 213, 51, 255),
+        new Color32(255, 192, 20, 255),
+        new Color32(245, 14, 0, 255),
+        new Color32(224, 91, 82, 255),
+        new Color32(184, 178, 176, 255)
     };
 }
